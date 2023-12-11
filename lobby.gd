bfead3a9f1622c8523ef508c037fae6a1e267b34
MilesTeg97 <64781653+MilesTeg97@users.noreply.github.com>
2023-12-11 16:25:31 +0100

    reset back to ENet demo (3.x branch)
    source  is most recent ENetMultiplayer demo from godotengine/godot-demo-projects/tree/3.x/networking/multiplayer_bomber  (3.x branch)
    
    todo:  re-apply patches to reach feature-parity with webrtc-version (2019, 3.1?).
    
    permalink:
    https://github.com/godotengine/godot-demo-projects/tree/bfcd81aa85005fb812b9e99eb804d19f32181d35/networking/multiplayer_bomber

--- lobby.gd
+++ lobby.gd
@@ -1,98 +1,93 @@
 extends Control
 
+
 func _ready():
-	if OS.get_name() == 'HTML5':
-		$connect/server.hide()
 	# Called every time the node is added to the scene.
 	gamestate.connect("connection_failed", self, "_on_connection_failed")
 	gamestate.connect("connection_succeeded", self, "_on_connection_success")
 	gamestate.connect("player_list_changed", self, "refresh_lobby")
-	gamestate.connect("lobby_joined", self, "_update_lobby")
 	gamestate.connect("game_ended", self, "_on_game_ended")
 	gamestate.connect("game_error", self, "_on_game_error")
-	if OS.get_name() == 'HTML5':
-		var data = JavaScript.eval("(new URLSearchParams(window.location.hash.replace('#', '', 1))).get('lobby')")
-		if typeof(data) == TYPE_STRING:
-			$connect/lobby.text = data
+	# Set the player name according to the system username. Fallback to the path.
+	if OS.has_environment("USERNAME"):
+		$Connect/Name.text = OS.get_environment("USERNAME")
+	else:
+		var desktop_path = OS.get_system_dir(0).replace("\\", "/").split("/")
+		$Connect/Name.text = desktop_path[desktop_path.size() - 2]
 
-func _update_lobby(text):
-	if OS.get_name() == 'HTML5':
-		JavaScript.eval("var x = new URLSearchParams(window.location.hash.replace('#', '', 1)); x.set('lobby', '" + text + "'); window.location.hash = x.toString()")
-	$players/lobby.text = text
 
 func _on_host_pressed():
-	if get_node("connect/name").text == "":
-		get_node("connect/error_label").text = "Invalid name!"
+	if $Connect/Name.text == "":
+		$Connect/ErrorLabel.text = "Invalid name!"
 		return
 
-	get_node("connect").hide()
-	get_node("players").show()
-	get_node("connect/error_label").text = ""
+	$Connect.hide()
+	$Players.show()
+	$Connect/ErrorLabel.text = ""
 
-	var player_name = get_node("connect/name").text
-	var ip = get_node("connect/ip").text
-	gamestate.host_game(player_name, ip)
-	# refresh_lobby() gets called by the player_list_changed signal, emitted when host is ready
+	var player_name = $Connect/Name.text
+	gamestate.host_game(player_name)
+	refresh_lobby()
+
 
 func _on_join_pressed():
-	if get_node("connect/name").text == "":
-		get_node("connect/error_label").text = "Invalid name!"
+	if $Connect/Name.text == "":
+		$Connect/ErrorLabel.text = "Invalid name!"
 		return
 
-	var ip = get_node("connect/ip").text
-	var lobby = get_node("connect/lobby").text
-	if lobby == '':
-		get_node("connect/error_label").text = "Must specify a lobby when joining!"
+	var ip = $Connect/IPAddress.text
+	if not ip.is_valid_ip_address():
+		$Connect/ErrorLabel.text = "Invalid IP address!"
 		return
-	get_node("connect/error_label").text=""
-	get_node("connect/host").disabled = true
-	get_node("connect/join").disabled = true
 
-	var player_name = get_node("connect/name").text
-	gamestate.join_game(ip, player_name, lobby)
-	# refresh_lobby() gets called by the player_list_changed signal
+	$Connect/ErrorLabel.text = ""
+	$Connect/Host.disabled = true
+	$Connect/Join.disabled = true
+
+	var player_name = $Connect/Name.text
+	gamestate.join_game(ip, player_name)
+
 
 func _on_connection_success():
-	get_node("connect").hide()
-	get_node("players").show()
+	$Connect.hide()
+	$Players.show()
+
 
 func _on_connection_failed():
-	get_node("connect/host").disabled = false
-	get_node("connect/join").disabled = false
-	get_node("connect/error_label").set_text("Connection failed.")
+	$Connect/Host.disabled = false
+	$Connect/Join.disabled = false
+	$Connect/ErrorLabel.set_text("Connection failed.")
+
 
 func _on_game_ended():
 	show()
-	get_node("connect").show()
-	get_node("players").hide()
-	get_node("connect/host").disabled = false
-	get_node("connect/join").disabled = false
-	if OS.get_name() == 'HTML5':
-		JavaScript.eval("var x = new URLSearchParams(window.location.hash.replace('#', '', 1)); x.delete('lobby'); window.location.hash = x.toString()")
-	$players/lobby.text = ''
-	$connect/lobby.text = ''
+	$Connect.show()
+	$Players.hide()
+	$Connect/Host.disabled = false
+	$Connect/Join.disabled = false
+
 
 func _on_game_error(errtxt):
-	get_node("error").dialog_text = errtxt
-	get_node("error").popup_centered_minsize()
+	$ErrorDialog.dialog_text = errtxt
+	$ErrorDialog.popup_centered_minsize()
+	$Connect/Host.disabled = false
+	$Connect/Join.disabled = false
+
 
 func refresh_lobby():
 	var players = gamestate.get_player_list()
 	players.sort()
-	get_node("players/list").clear()
-	get_node("players/list").add_item(gamestate.get_player_name() + " (You)")
+	$Players/List.clear()
+	$Players/List.add_item(gamestate.get_player_name() + " (You)")
 	for p in players:
-		get_node("players/list").add_item(p)
+		$Players/List.add_item(p)
 
-	get_node("players/start").disabled = not get_tree().is_network_server()
+	$Players/Start.disabled = not get_tree().is_network_server()
+
 
 func _on_start_pressed():
 	gamestate.begin_game()
 
-func _on_server_toggled(button_pressed):
-	if button_pressed:
-		Server.listen(9080)
-		$connect/server.text = "Stop"
-	else:
-		Server.stop()
-		$connect/server.text = "Listen"
+
+func _on_find_public_ip_pressed():
+	OS.shell_open("https://icanhazip.com/")
--- player.gd
+++ player.gd
@@ -1,5 +1,6 @@
 extends KinematicBody2D
 
+
 const MOTION_SPEED = 90.0
 
 puppet var puppet_pos = Vector2()
@@ -7,18 +8,15 @@ puppet var puppet_motion = Vector2()
 
 export var stunned = false
 
-# Use sync because it will be called everywhere
-sync func setup_bomb(bomb_name, pos, by_who):
-	var bomb = preload("res://bomb.tscn").instance()
-	bomb.set_name(bomb_name) # Ensure unique name for the bomb
-	bomb.position = pos
-	bomb.from_player = by_who
-	# No need to set network mode to bomb, will be owned by master by default
-	get_node("../..").add_child(bomb)
-
 var current_anim = ""
 var prev_bombing = false
 var bomb_index = 0
+
+
+func _ready():
+	stunned = false
+	puppet_pos = position
+
 
 func _physics_process(_delta):
 	var motion = Vector2()
@@ -40,7 +38,7 @@ func _physics_process(_delta):
 			motion = Vector2()
 
 		if bombing and not prev_bombing:
-			var bomb_name = get_name() + str(bomb_index)
+			var bomb_name = String(get_name()) + str(bomb_index)
 			var bomb_pos = position
 			rpc("setup_bomb", bomb_name, bomb_pos, get_tree().get_network_unique_id())
 
@@ -74,8 +72,20 @@ func _physics_process(_delta):
 	if not is_network_master():
 		puppet_pos = position # To avoid jitter
 
+
+# Use sync because it will be called everywhere
+remotesync func setup_bomb(bomb_name, pos, by_who):
+	var bomb = preload("res://bomb.tscn").instance()
+	bomb.set_name(bomb_name) # Ensure unique name for the bomb
+	bomb.position = pos
+	bomb.from_player = by_who
+	# No need to set network master to bomb, will be owned by server by default
+	get_node("../..").add_child(bomb)
+
+
 puppet func stun():
 	stunned = true
+
 
 master func exploded(_by_who):
 	if stunned:
@@ -83,9 +93,6 @@ master func exploded(_by_who):
 	rpc("stun") # Stun puppets
 	stun() # Stun master - could use sync to do both at once
 
+
 func set_player_name(new_name):
 	get_node("label").set_text(new_name)
-
-func _ready():
-	stunned = false
-	puppet_pos = position
